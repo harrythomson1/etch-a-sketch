f7390474e4a2c6bf4120f92e579e84d342a86e90
Harry Thomson <haroldt95@gmail.com>
2020-08-15 10:12:17 +0100

    Created custome grid size functonality.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,50 @@
 let container = document.querySelector("#container");
 let reset = document.querySelector("#reset")
+let newGrid = document.querySelector("#newGrid")
+createDefault()
 
 //Creates 16X16 grid and adds functionality.
 function createDefault() {
     for (i = 0; i < 256; i++) {
-        let gridBox = document.createElement("div")
-        gridBox.classList.add("row")
-        container.appendChild(gridBox)
-        gridBox.addEventListener("mouseover", function(){draw(gridBox)})
-        reset.addEventListener("click", function(){resetBtn (gridBox)})
+        let gridContainer = document.createElement("div")
+        gridContainer.classList.add("row")
+        container.appendChild(gridContainer)
+        gridContainer.addEventListener("mouseover", function(){draw(gridContainer)})
+        reset.addEventListener("click", function(){resetBtn (gridContainer)})
     }
 }
 
-createDefault()
 
 //Adds the draw class to the DIV's
 function draw (elem) {
-    elem.classList.add("draw")
+    elem.style.backgroundColor = `rgb(${getRandomInt(357)}, ${getRandomInt(357)}, ${getRandomInt(357)})`
 }
 
 //Resets the grid
 function resetBtn (elem) {
-    elem.classList.remove("draw")
+    elem.style.removeProperty("background-Color");
 }
+
+//Generates a random integer 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function createNew (num) {
+    let gridBox = document.querySelectorAll(".row")
+    newSize = num * num
+    for (i = 0; i < gridBox.length; i++) {
+        gridBox[i].remove();
+    }
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr`
+    for (i = 0; i < newSize; i++) {
+        let gridContainer = document.createElement("div")
+        gridContainer.classList.add("row")
+        container.appendChild(gridContainer)
+        gridContainer.addEventListener("mouseover", function(){draw(gridContainer)})
+        reset.addEventListener("click", function(){resetBtn (gridContainer)})
+    }
+}
+
+newGrid.addEventListener("click", function (){createNew (prompt("Please select a Grid size. (Input 64 for a 64 x 64 Sketchpad)"))})
